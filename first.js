6dfe12e435212facbb34cc37e856243468fa14c8
FIDHA <fidhasherin.eranhikkal@gmail.com>
2025-10-07 08:57:33 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,67 @@
+// console.log(`HELLO`);
+// console.log(`my name is fidha sherin`);
+// window.alert(`i like pizza`);
+// let x=123;
+// console.log(x);
+// let age= 25;
+// console.log(age);
+
+// let name = 34;
+// let price = 12;
+// let gpa = 2.1;
+// console.log(typeof name);
+// console.log(`you are ${name} is years old`);
+// console.log(`The price is ${price}`);
+
+// let firstName ="fidha";
+// console.log(firstName);
+// document.getElementById("miP").textContent = firstName;
+//  let username;
+//  username = window.prompt("what is your name");
+//  console.log(username);
+
+// let username;
+// document.getElementById("submit").onclick =function(){
+//     username =document.getElementById("inp").value;
+//     // console.log(username);\
+// document.getElementById("minna").textContent =`hello ${username}`;
+// }
+
+//  type conversion = change the  datatype of a value to another
+//  (strings, numbers, booleans)
+// let age = window.prompt("how old are u?");
+
+// age =Number(age);
+// age+=1;
+// console.log(age);
+// constant
+
+// area of circle
+// const PI= 3.14;
+// let raduis;
+//  raduis=document.getElementById("mysubmit").onclick =function(){
+//     raduis = document.getElementById("myText").value;
+//          raduis = Number(raduis);
+
+//     circumference = 2 * PI* raduis;
+//     document.getElementById("he").textContent =`circumference is ${circumference};
+
+// }
+// // raduis = window.prompt("enter the raduis");
+// // raduis = Number(raduis);
+// // circumference = 2 * PI* raduis;
+
+// math:build- iN oBJEct that provides a ColleCtion oF propeRties and methods
+// console.log(Math.PI);
+// let x =3.4;
+// y =Math.round(x);
+// a =Math.floor(x);
+// b=Math.ceil(x);
+// c=Math.trunc(x);
+// console.log(y);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let randomnum =Math.floor(Math.random()*8);
+// console.log(randomnum);
